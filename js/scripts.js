250a6fca48d4d65ff687e858345dead2dc5c31c4
Geoff Winner <Geoff@Geoffs-MacBook-Pro.local>
2015-04-22 23:12:24 -0700

    fix js audio function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-// var audio = new Audio('js/sounds/menu.mp3');
-// audio.addEventListener('ended', function() {
-//     this.currentTime = 0;
-//     this.play();
-// }, false);
-// audio.play();
+var audio = new Audio('js/sounds/menu.mp3');
+audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+audio.play();
