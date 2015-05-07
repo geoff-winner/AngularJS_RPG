c4c2995a2be3de075bd5f3a5343c88c7e68da05f
geoff winner & brett moore & tanner heffner & connor abdelnoor <student@epicodus.com>
2015-05-07 14:17:44 -0700

    refactor app.js and level 1 fight begin

--- app.js
+++ app.js
@@ -30,80 +30,10 @@ characterList.config(function($stateProvider) {
       controller: 'CharCtrl'
   });
 
-
-
-
-//old routes?
-  $stateProvider.state('warrior', {
-    url: "/warrior",
-    templateUrl: "partials/warrior.html",
-    controller: 'WarriorCtrl'
-  });
-
-  $stateProvider.state('mage', {
-    url: "/mage",
-    templateUrl: "partials/mage.html",
-    controller: 'MageCtrl'
-  });
-
-  $stateProvider.state('rogue', {
-    url: "/rogue",
-    templateUrl: "partials/rogue.html",
-    controller: 'RogueCtrl'
-  });
-
-  $stateProvider.state('magename', {
-    url:"/CharacterStats",
-    templateUrl: "partials/charscreen.html",
-    controller: 'MageCtrl',
-  });
-
-  $stateProvider.state('roguename', {
-    url:"/CharacterStats",
-    templateUrl: "partials/charscreen.html",
-    controller: 'RogueCtrl',
-  });
-
-  $stateProvider.state('warriorname', {
-    url:"/CharacterStats",
-    templateUrl: "partials/charscreen.html",
-    controller: 'WarriorCtrl',
-  });
-
-  $stateProvider.state('level1war', {
-    url:"/level1",
-    templateUrl: "partials/level1.html",
-    controller: 'WarriorCtrl'
-  });
-
-  $stateProvider.state('level1mage', {
-    url:"/level1",
-    templateUrl: "partials/level1.html",
-    controller: 'MageCtrl'
-  });
-
-  $stateProvider.state('level1rogue', {
-    url:"/level1",
-    templateUrl: "partials/level1.html",
-    controller: 'RogueCtrl'
-  });
-
-  $stateProvider.state('level1warfight', {
-    url:"/level1",
-    templateUrl: "partials/level1fight.html",
-    controller: 'WarriorCtrl'
-  });
-
-  $stateProvider.state('level1magefight', {
-    url:"/level1",
-    templateUrl: "partials/level1fight.html",
-    controller: 'MageCtrl'
-  });
-
-  $stateProvider.state('level1roguefight', {
-    url:"/level1",
-    templateUrl: "partials/level1fight.html",
-    controller: 'RogueCtrl'
+  $stateProvider.state('level1', {
+      url: "/level1",
+      templateUrl: "partials/level1.html",
+      controller: 'CharCtrl'
   });
 
 });
